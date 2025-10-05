5100478585c422534796909eae49fba80353cc78
RaulRHLM23 <a339793@alumnos.uaslp.mx>
2025-10-05 00:19:32 -0600

    Connect knowledge graph with the service

--- SpaceNexus_Frontend/src/components/pages/KnowledgeGraph.jsx
+++ SpaceNexus_Frontend/src/components/pages/KnowledgeGraph.jsx
@@ -1,22 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Graph } from '../Graph'
 import { InfoNode } from '../InfoNode';
 import { useParams } from 'react-router-dom';
+
+import { getFullGraph } from '../../services/graph.service';
 
 import graphData from '../../assets/graph-data.json';
 import CustomizedInputBase from '../CustomizedInputBase';
 
 export function KnowledgeGraph() {
-  const [selectedNode, setSelectedNode] = React.useState(null);
+  const [graphData, setGraphData] = useState({ nodes: [], links: [] });
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+
+  const [selectedNode, setSelectedNode] = useState(null);
   const { user } = useParams();
 
-  React.useEffect(() => {
-    if (user) {
+
+  useEffect(() => {
+    const fetchGraphData = async () => {
+      try {
+        const data = await getFullGraph();
+        setGraphData(data);
+        console.log(data);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+      finally { setLoading(false); }
+    };
+    fetchGraphData();
+  }, []);
+  /*
+    useEffect(() => {
+      if (user) {
+        const node = graphData.nodes.find(n => n.id === user);
+        if (node) setSelectedNode(node);
+      }
+    }, [user]);
+  */
+  useEffect(() => {
+    // Nos aseguramos de que los nodos ya se hayan cargado antes de buscar
+    if (user && graphData.nodes.length > 0) {
       const node = graphData.nodes.find(n => n.id === user);
       if (node) setSelectedNode(node);
     }
-  }, [user]);
-
+  }, [user, graphData]);
   const handleRelationClick = (relationId) => {
     const relatedNode = graphData.nodes.find(node => node.id === relationId);
     if (relatedNode) {
